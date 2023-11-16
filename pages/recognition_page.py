8117ad66e03f28e84c300b39f856e3e1867d0025
fhoang <phanhuyhoang0425@gmail.com>
2023-11-17 01:57:18 +0700

    upload

--- pages/recognition_page.py
+++ pages/recognition_page.py
@@ -1,4 +1,26 @@
 import streamlit as st
+from PIL import Image
+import numpy as np
+from app import App
+import pymongo
 
+app = App()
 st.markdown("# Face Recognition")
 st.sidebar.markdown("# Face Recognition")
+
+img_file_buffer = st.camera_input("Take a photo to recognize")
+
+submit_button = st.button("Submit")
+
+if submit_button:
+  if img_file_buffer is not None:
+          # To read the image file buffer as a PIL Image:
+          
+          img_buffer = Image.open(img_file_buffer)
+
+          # To convert PIL Image to a numpy array:
+          img_array = np.array(img_buffer)
+          pid, score = app.recognize(img_array)
+          person = app.get_person_info(pid)
+          
+
